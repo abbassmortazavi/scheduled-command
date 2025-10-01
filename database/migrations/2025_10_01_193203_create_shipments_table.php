500f4544dd5f64c165446fa1e1c39182d4cacc4b
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-10-01 23:11:51 +0330

    feat: create model and migration,command

---
+++ database/migrations/2025_10_01_193203_create_shipments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('tracking_number')->unique();
+            $table->string('status')->default('in_progress');
+            $table->timestamp('last_status_check')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+
+            $table->index('status');
+            $table->index(['status', 'last_status_check']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipments');
+    }
+};
